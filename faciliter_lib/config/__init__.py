903a8ac0cdf45d39d66c39d481040e53c7d23afe
Julien Nadaud <nad.julien@gmail.com>
2025-09-07 11:30:30 +0200

    Refactored categories into doc_categories

--- faciliter_lib/config/__init__.py
+++ faciliter_lib/config/__init__.py
@@ -3,6 +3,6 @@
 Expose category constants so applications can import them as
 `from faciliter_lib import DOC_CATEGORIES` or `from faciliter_lib.config import DOC_CATEGORIES`.
 """
-from .categories import DOC_CATEGORIES, DOC_CATEGORIES_BY_KEY, DOC_CATEGORY_CHOICES
+from .doc_categories import DOC_CATEGORIES, DOC_CATEGORIES_BY_KEY, DOC_CATEGORY_CHOICES
 
 __all__ = ["DOC_CATEGORIES", "DOC_CATEGORIES_BY_KEY", "DOC_CATEGORY_CHOICES"]
